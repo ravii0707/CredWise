2ac2550ee1526b550f081f8bf93f68d75e3d54c5
raviteja <rkundaram@vivejaitservices.com>
2025-05-27 12:14:34 +0530

    email

--- CredWiseAdmin.Utils/obj/Debug/net8.0/CredWiseAdmin.Utils.AssemblyInfo.cs
+++ CredWiseAdmin.Utils/obj/Debug/net8.0/CredWiseAdmin.Utils.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CredWiseAdmin.Utils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4ae215de351419d82216e08acbf7edaa3cf7302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe72346800d3491fa0cbee15771b0400ac162823")]
 [assembly: System.Reflection.AssemblyProductAttribute("CredWiseAdmin.Utils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CredWiseAdmin.Utils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
